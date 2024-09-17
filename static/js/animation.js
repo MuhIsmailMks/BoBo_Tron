f7b200a7f3c8e84cf974cc9d0539c8513a4e3ce4
muhammad ismail <muhismailmks@gmail.com>
2024-09-17 09:51:51 +0800

    update

--- static/js/animation.js
+++ static/js/animation.js
@@ -19,13 +19,58 @@ function initRocketAnimation() {
     );
 }
 
+function initBoxAnimation() { 
+    ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    gsap.killTweensOf('.box_fix');
+
+    if (window.innerWidth > 1024) { 
+        ScrollTrigger.create({
+            trigger: ".box_fix",  
+            pin: true,
+            start: "top top",
+            end: "+=700 top",
+            scrub: 3,
+        });
+    } else { 
+        gsap.fromTo('.box_fix', 
+            { y: '10vw' }, 
+            { y: '-100vw', 
+              ease: "none", 
+              scrollTrigger: {
+                trigger: '.howToBuyContainer',
+                start: "50% 100%",
+                end: "100% 50%",
+                scrub: 5,
+              }
+            }
+        );
+    }
+}
+
+// Fungsi debounce untuk event resize
+function debounce(func, wait = 100) {
+    let timeout;
+    return function(...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+}
+
 // Jalankan animasi saat DOM siap
 window.addEventListener('DOMContentLoaded', () => {
     initRocketAnimation(); 
+    initBoxAnimation();
+
+    // new TypeIt(".typeAnimation", { 
+    //     speed: 175,
+    //     loop: false,
+    //   }).go();
+ 
 });
 
 // Tambahkan listener untuk resize
 window.addEventListener('resize', () => { 
     initRocketAnimation(); 
+    initBoxAnimation();
     ScrollTrigger.refresh();
 });
