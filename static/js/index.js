a2c75475386da25b8eea8d2379b8d71d01758e28
muhammad ismail <muhismailmks@gmail.com>
2024-10-08 18:41:31 +0800

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,3 @@
-// no inspect element
-
-// document.addEventListener('contextmenu', function(e) {
-//     e.preventDefault();
-// }, false);
-
-// document.onkeydown = function(e) {
-//     if (e.keyCode == 123 || // F12 key
-//         (e.ctrlKey && e.shiftKey && e.keyCode == 73) || // CTRL+SHIFT+I
-//         (e.ctrlKey && e.shiftKey && e.keyCode == 74)) { // CTRL+SHIFT+J
-//         return false;
-//     }
-// };
-
-
-
-
 
 //  scroll animation Effect
 window.addEventListener("DOMContentLoaded", () => {
